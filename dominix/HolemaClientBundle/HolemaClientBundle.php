774fa266f37c1c29f6d22be9cd51d2e33f432146
dsa <dominik.sander@googlemail.com>
2017-08-12 21:46:57 +0200

    initial commit

---
+++ dominix/HolemaClientBundle/HolemaClientBundle.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of the HolemaClientBundle Bundle.
+ *
+ * (c) Dominik Sander <https://dominix-design.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace dominix\HolemaClientBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Configures the HolemaClientBundle Bundle.
+ */
+class HolemaClientBundle extends Bundle
+{
+}
