7864cabd04a79412906be54b1c98fc30d17cebc5
dsa <dmc-dsa@users.noreply.github.com>
2017-08-24 15:59:55 +0200

    add headline to simplebox

--- dominix/HolemaClientBundle/ContentElements/SimpleBox.php
+++ dominix/HolemaClientBundle/ContentElements/SimpleBox.php
@@ -4,6 +4,23 @@ namespace dominix\HolemaClientBundle\ContentElements;
 class SimpleBox extends \ContentElement {
     protected $strTemplate = 'ce_simplebox';
 
+		public function generate()
+		{
+			if (TL_MODE == 'BE')
+			{
+				/** @var \BackendTemplate|object $objTemplate */
+				$objTemplate = new \BackendTemplate('be_wildcard');
+				$objTemplate->wildcard = '### SIMPLEBOX ###';
+				$objTemplate->title = $this->headline;
+				$objTemplate->id = $this->id;
+				$objTemplate->link = $this->name;
+				$objTemplate->href = 'contao/main.php?do=themes&amp;table=tl_module&amp;act=edit&amp;id=' . $this->id;
+				return $objTemplate->parse();
+			}
+
+			return parent::generate();
+		}
+
     protected function compile()
     {
 
--- dominix/HolemaClientBundle/Resources/contao/dca/tl_content.php
+++ dominix/HolemaClientBundle/Resources/contao/dca/tl_content.php
@@ -1,3 +1,3 @@
 <?php
 
-$GLOBALS['TL_DCA']['tl_content']['palettes']['simplebox'] = '{type_legend},type;{link_legend},singleSRC,url,titleText;{text_legend},html;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests;{invisible_legend:hide},invisible,start,stop';
+$GLOBALS['TL_DCA']['tl_content']['palettes']['simplebox'] = '{type_legend},type;{link_legend},headline,singleSRC,url,titleText;{text_legend},html;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests;{invisible_legend:hide},invisible,start,stop';
