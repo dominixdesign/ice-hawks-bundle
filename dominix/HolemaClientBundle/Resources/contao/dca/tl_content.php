5c1b3fc982d91211051e062b0283a9eca9734b83
dsa <dominik.sander@googlemail.com>
2017-12-17 12:58:29 +0100

    add css class to people box

--- dominix/HolemaClientBundle/Resources/contao/dca/tl_content.php
+++ dominix/HolemaClientBundle/Resources/contao/dca/tl_content.php
@@ -6,7 +6,8 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['people_attributes'] = array
 	 'exclude'                 => true,
 	 'search'                  => true,
 	 'inputType'               => 'keyValueWizard',
-	 'sql'                     => "text NULL"
+	 'sql'                     => "text NULL",
+	 'eval'                    => array('tl_class'=>'clr')
 );
 
 //simplebox
@@ -17,6 +18,6 @@ $GLOBALS['TL_DCA']['tl_content']['palettes']['rowStart'] = '{type_legend},type;{
 $GLOBALS['TL_DCA']['tl_content']['palettes']['rowStop'] = '{type_legend},type;{template_legend:hide},customTpl;';
 
 // people box
-$GLOBALS['TL_DCA']['tl_content']['palettes']['people'] = '{type_legend},type;{link_legend},headline,people_attributes;{image_legend},singleSRC;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests;{invisible_legend:hide},invisible,start,stop';
+$GLOBALS['TL_DCA']['tl_content']['palettes']['people'] = '{type_legend},type;{link_legend},headline,people_attributes;{image_legend},singleSRC;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID;{invisible_legend:hide},invisible,start,stop';
 
 $GLOBALS['TL_DCA']['tl_content']['palettes']['ad'] = '{type_legend},type;{link_legend},headline,html;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID;{invisible_legend:hide},invisible,start,stop';
