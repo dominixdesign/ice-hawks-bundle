8f1af58c8f2cd1fcdeedb5a7b70e4d3fbfe13e35
Dominik Sander <dominik.sander@diconium.com>
2018-08-03 07:50:45 +0200

    rename

---
+++ dominix/IceHawksBundle/ContaoManager/Plugin.php
@@ -0,0 +1,32 @@
+<?php
+ declare(strict_types=1);
+/*
+ * This file is part of the IceHawksBundle.
+ *
+ * (c) Dominik Sander <http://dominix-design.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+ namespace Contao\NewsBundle\ContaoManager;
+ use Contao\CoreBundle\ContaoCoreBundle;
+ use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+ use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+ use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+ use Contao\IceHawksBundle\IceHawksBundle;
+
+ class Plugin implements BundlePluginInterface
+ {
+     /**
+      * {@inheritdoc}
+      */
+     public function getBundles(ParserInterface $parser): array
+     {
+         return [
+             BundleConfig::create(IceHawksBundle::class)
+                 ->setLoadAfter([ContaoCoreBundle::class])
+                 ->setReplace(['icehawks']),
+         ];
+     }
+ }
